c1b4bf2a89dc78fec3409ea1f7405a73f69c982b
georapbox <georapbox@gmail.com>
2021-06-21 22:18:29 +0300

    do not close popup after selection

--- src/switcher.js
+++ src/switcher.js
@@ -3,13 +3,12 @@ const themeSelect = document.getElementById('theme-select');
 function enableTheme(evt) {
   evt.preventDefault();
   browser.management.setEnabled(evt.target.value, true);
-  window.close();
 }
 
-browser.management.getAll().then((extensions) => {
-  for (let extension of extensions) {
+browser.management.getAll().then(extensions => {
+  for (const extension of extensions) {
     if (extension.type === 'theme') {
-      let option = document.createElement('option');
+      const option = document.createElement('option');
 
       option.textContent = extension.name;
       option.title = extension.description;
