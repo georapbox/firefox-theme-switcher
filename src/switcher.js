a756651b5ef4c2c2180f5f1a3db43ed288f9eca6
Thomas Bertels <3265870+tbertels@users.noreply.github.com>
2023-04-27 09:32:57 +0200

    Sort themes by name
    Current theme at the top

--- src/switcher.js
+++ src/switcher.js
@@ -6,20 +6,36 @@ function enableTheme(evt) {
 }
 
 browser.management.getAll().then(extensions => {
+  var themes = [];
+  
   for (const extension of extensions) {
     if (extension.type === 'theme') {
-      const option = document.createElement('option');
+      if (extension.enabled) { // current theme at the top
+        const option = document.createElement('option');
 
-      option.textContent = extension.name;
-      option.title = extension.description;
-      option.value = extension.id;
-
-      if (extension.enabled) {
+        option.textContent = extension.name;
+        option.title = extension.description;
+        option.value = extension.id;
         option.selected = true;
+        
+        themeSelect.appendChild(option);
       }
-
-      themeSelect.appendChild(option);
+      else {
+        themes.push([extension.name, extension.description, extension.id, extension.enabled]);
+      }
     }
+  }
+  themes.sort();
+  
+  for (const theme of themes) {
+    const option = document.createElement('option');
+
+    option.textContent = theme[0];
+    option.title = theme[1];
+    option.value = theme[2];
+    option.selected = theme[3];
+
+    themeSelect.appendChild(option);
   }
 });
 
